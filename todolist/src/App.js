e18d25e835d9708fc2805ae0d125dae688917566
Anna Gerasimenko <annagerasimenko@MacBook-Pro-Anuar.local>
2023-09-23 23:18:20 +0600

    init: start to do list

---
+++ todolist/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  debugger;
+  return (
+    <div className="App">
+      <Todolist />
+    </div>
+  );
+}
+
+function Todolist() {
+  debugger;
+  return (
+  <div>TODO LIST</div>
+  );
+}
+
+export default App;
